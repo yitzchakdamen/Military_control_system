436ace177fe5f557e87fc124f893bf094179a2f5
yitzchakdamen <yitzchakdamen@gmail.com>
2025-05-23 00:04:07 +0300

    Step 1: Creating the organizations

---
+++ Military_control_system/Organization.cs
@@ -0,0 +1,31 @@
+namespace Military_control_system
+{
+    class Organization
+    {
+        string EstablishmentDate, CurrentCommander, Name; // Date of establishment of the organization and current commander
+        public Organization(string name, string establishmentDate, string currentCommander)
+        {
+            EstablishmentDate = establishmentDate;
+            CurrentCommander = currentCommander;
+            Name = name;
+            Print();
+        }
+
+        public void Print()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"\n =====================>> {Name} <<=====================");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("    The organization was created successfully.");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"    Date of establishment: {EstablishmentDate}");
+            Console.WriteLine($"    Current census: {CurrentCommander}");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(" =============================================\n");
+            Console.ResetColor();
+        }
+    }
+}
--- Military_control_system/Program.cs
+++ Military_control_system/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Military_control_system
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Organization Hamas = new("Hamas", "1987.12.10", "Yahya Sinwar");
+            Organization Idf = new ("IDF","1948.5.26", "Herzi Halevi");
+        }   
+    }    
+}
